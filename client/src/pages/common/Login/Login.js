f812f13a3ed95601bc6b46124e6ac4fb329a3fc6
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-08 00:36:02 +0200

    Login design

--- client/src/pages/common/Login/Login.js
+++ client/src/pages/common/Login/Login.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Form } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const onFinish = (value) => {
+        console.log(value);
+    }
+    
     return (
         <div className='flex justify-center items-center h-screen w-screen'>
             <div className='card w-400 p-4'>
                 <div className='flex flex-col'>
                     <h1 className='text-3xl'>Login</h1>
                     <div className="divider"></div>
-                    <Form layout='vertical' >
+                    <Form layout='vertical' onFinish={onFinish}>
                         <Form.Item
                             name='email'
                             label='Email'
@@ -23,7 +28,10 @@ const Login = () => {
                             <input type='password' />
                         </Form.Item>
                         <br />
-                        <button type="submit" className='primary-contained-btn w-100'>Login</button>
+                        <div className='flex flex-col gap-2'>
+                            <button type="submit" className='primary-contained-btn w-100 text-xl'>Login</button>
+                            <Link to='/register' className='text-lg'>Not a Member? Register</Link>
+                        </div>
                     </Form>
                 </div>
             </div>
