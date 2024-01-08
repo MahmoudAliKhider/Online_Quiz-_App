1a8455c290052ef692103eda596b188b307d803d
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-08 16:31:37 +0200

    mongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const dbConfig = require('./config/dbConfig');
+
 
 const app = express();
 app.use(express.json());
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 })
