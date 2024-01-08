380855317e4e0488a2a90426c22671d79c6d375a
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-08 23:57:07 +0200

    add components to sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import Login from "./pages/common/Login/Login";
-import Register from "./pages/common/Register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./styleSheets/alignments.css";
 import "./styleSheets/textElements.css";
 import "./styleSheets/theme.css";
 import "./styleSheets/custom-components.css";
 import "./styleSheets/form-elements.css";
+import "./styleSheets/layout.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectRoute from "./components/protectRoute";
+import Login from "./pages/common/Login/Login";
+import Register from "./pages/common/Register/Register";
 import Home from "./pages/common/Home/Home";
+
+import ProtectRoute from "./components/protectRoute";
+
 
 function App() {
   return (
--- client/src/components/protectRoute.js
+++ client/src/components/protectRoute.js
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { message } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { getUserInfo } from '../apiCalls/users'
 import { setUser } from "../redux/userSlice";
 
 const ProtectRoute = ({ children }) => {
-const {user }= useSelector((state) => state.users);
+  const { user } = useSelector((state) => state.users);
+  const [menu, setMenu] = useState([]);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const getUserData = async () => {
     try {
       const response = await getUserInfo();
 
       if (response.success) {
-        message.success(response.message);
-        dispatch(setUser(response.data))
+        // message.success(response.message);
+        dispatch(setUser(response.data));
+        if (response.data.isAdmin) {
+          setMenu(adminMenu);
+        } else {
+          setMenu(userMenu);
+        }
       } else {
         message.error(response.message);
       }
@@ -29,11 +37,78 @@ const {user }= useSelector((state) => state.users);
     getUserData();
   }, []);
 
+
+  const userMenu = [
+    {
+      name: "Home",
+      paths: ["/"],
+      icon: <i className="ri-home-4-line"></i>,
+      onclick: () => navigate("/")
+    },
+    {
+      name: "Reports",
+      paths: ["/reports"],
+      icon: <i className="ri-file-chart-line"></i>,
+      onClick: () => navigate("/reports")
+    },
+    {
+      name: "Profile",
+      paths: ["/profile"],
+      icon: <i className="ri-file-user-line"></i>,
+      onClick: () => navigate("/profile")
+    },
+    {
+      name: "LogOut",
+      paths: ["/logout"],
+      icon: <i className="ri-logout-circle-r-line"></i>,
+      onClick: () => {
+        localStorage.removeItem("token");
+        navigate("/login")
+      }
+    }
+  ];
+
+  const adminMenu = [];
+
+  const atchiveRoute = window.location.pathname;
+
   return (
-    <div>
-    {user?.name}
-      
-      {children}</div>
+    <div className='layout'>
+      <div className='flex gap-2 w-full h-full h-100'>
+        <div className='sidebar text-white'>
+          <div className='menu'>
+            {
+              menu.map((item, index) => {
+                return <div className=
+                  {`menu-item ${atchiveRoute === item.paths[0] && "active-menu-item"}`}
+                  key={index}
+                  onClick={item.onClick}
+                >
+                  {item.icon}
+                  <span>{item.name}</span>
+                </div>
+              })
+            }
+          </div>
+        </div>
+        <div className='body'>
+          <div className='header flex justify-between'>
+            <i className="ri-close-circle-line"></i>
+            <h1 className='text-2xl '>Master Quiz</h1>
+            <div>
+              <div className="flex gap-1 items-center">
+                <h1 className="text-md text-white">{user?.name}</h1>
+              </div>
+              <span>Role : {user?.isAdmin ? "Admin" : "User"}</span>
+            </div>
+
+          </div>
+          <div className='content'>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
