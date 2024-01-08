1986d9cd6183928837226f2fe8639fb71d19cc5a
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-08 22:05:20 +0200

    auth middleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.autherization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.body.userId = decodedToken.userId;
+        next();
+    } catch (error) {
+        res.status(401).send({
+            message: "You are not authenticated",
+            data: error,
+            success: false,
+        })
+    }
+}
