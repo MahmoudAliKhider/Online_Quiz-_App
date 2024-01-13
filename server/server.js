f0914aeff64a380891f1a8f4616c62761b1f2701
MahmoudAliKhider <mahmoudkhider37@gmail.co>
2024-01-13 18:34:52 +0200

    production

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const path = require('path');
 
 const dbConfig = require('./config/dbConfig');
 const userRouter = require('./routes/usersRoute');
@@ -11,9 +12,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname + './client/build')))
+
 app.use('/api/users', userRouter);
 app.use('/api/exams', examRouter);
 app.use('/api/reports', reportRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
